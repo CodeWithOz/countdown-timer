508cb9ac8f837b6136e37aed57845cb9a90e5eb9
Uche Ozoemena <ucheozoemena@gmail.com>
2020-04-19 22:55:10 +0100

    Add listener that prevents non-numeric input for the duration

---
+++ js/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    initialize();
+}, false);
+
+function initialize() {
+    let curVal = '';
+    document.querySelector('.duration-container input').addEventListener('keyup', e => {
+        const newVal = e.target.value;
+        if (newVal === curVal) {
+            resetFieldToLastValue(e.target);
+            return;
+        }
+        const asNumber = Number(newVal);
+        if (Number.isNaN(asNumber) || asNumber < 1) {
+            resetFieldToLastValue(e.target);
+            return;
+        }
+        if (newVal.length > 6) {
+            // invalid number of characters
+            resetFieldToLastValue(e.target);
+            return;
+        }
+    }, false);
+
+    function resetFieldToLastValue(field) {
+        field.value = curVal;
+    }
+}
